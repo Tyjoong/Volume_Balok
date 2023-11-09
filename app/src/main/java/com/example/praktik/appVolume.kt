77c90c2601ddd2379c7234019de3756d6f4d43a1
Tyjoong <csalma251@gmail.com>
2023-11-09 08:55:12 +0700

    Volume Balok

---
+++ app/src/main/java/com/example/praktik/appVolume.kt
@@ -0,0 +1,11 @@
+package com.example.praktik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class appVolume : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_volume)
+    }
+}
